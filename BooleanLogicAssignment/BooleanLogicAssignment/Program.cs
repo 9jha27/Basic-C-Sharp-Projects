ebb57d22cdc82267eb2bc2299606edd5b347af66
9jha27 <65628178+9jha27@users.noreply.github.com>
2020-08-26 11:36:30 -0600

    Boolean Logic Assignment

---
+++ BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Approval Program for Car Insurance");
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI?");
+            string dui = Console.ReadLine();
+         
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qualified for car insurance?");
+            bool qualified = age > 15 && dui == "no" && tickets <= 3;
+            Console.WriteLine(qualified);
+           
+
+            Console.ReadLine();
+        }
+    }
+}

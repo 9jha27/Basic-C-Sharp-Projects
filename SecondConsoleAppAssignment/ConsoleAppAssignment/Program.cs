795d4470a257fce8749af7bac07890fc170afa7b
9jha27 <65628178+9jha27@users.noreply.github.com>
2020-08-27 21:07:43 -0600

    my second conosle app with stringbuilders

---
+++ SecondConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string beginning = "This is the beginning, ";
+            string middle = "the middle, ";
+            string end = "and the end!";
+
+            string together = beginning + middle + end;
+            Console.WriteLine(together); //three strings concatenated
+            
+
+            together = together.ToUpper(); //to Uppercase
+            Console.WriteLine(together);
+            Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder(); //creating an instance of a stringbuilder class
+            sb.Append("This is the first sentence of my StringBuilder.");
+            sb.Append("\nThis is the second sentence of my StringBuilder.");
+            sb.Append("\nThe third...");
+            sb.Replace("the", "my");
+            sb.Append("\nIs this what I am supposed to be doing in this assignment?");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+        }
+    }
+}

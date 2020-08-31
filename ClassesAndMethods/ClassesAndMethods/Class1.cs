0e3779eaa4cd73f4ab7f5eaf0ec1c3163b081f3d
9jha27 <65628178+9jha27@users.noreply.github.com>
2020-08-31 16:39:51 -0600

    Methods and Classes assignment

---
+++ ClassesAndMethods/ClassesAndMethods/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethods
+{
+    public class MathOperation //inside our class "Math", we have ...
+    {
+        public static int Add(int age) //a method called add...
+        {
+            int add = age + 5;
+            return add;
+        }
+        public static int Multiply(int age)
+        {
+            int multiply = age * 5;
+            return multiply;
+        }
+        public static double Divide(double age)
+        {
+            double divide = age / 5;
+            return divide;
+        }
+
+    }
+}
---
+++ ClassesAndMethods/ClassesAndMethods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            int add = MathOperation.Add(age);
+            Console.WriteLine("You will be " + add + " years old in five years!");
+
+            int multiply = MathOperation.Multiply(age);
+            Console.WriteLine("Five times your age is " + multiply);
+
+            double divide = MathOperation.Divide(age);
+            Console.WriteLine("Your age divided by 5 is " + divide);
+
+            Console.ReadLine();
+
+        }
+    }
+}

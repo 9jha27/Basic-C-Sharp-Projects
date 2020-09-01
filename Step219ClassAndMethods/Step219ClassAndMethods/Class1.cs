f8bba87bcc041df4afc1905da1955443de990ad1
9jha27 <65628178+9jha27@users.noreply.github.com>
2020-09-01 13:14:40 -0600

    MethodOverloading assignment

---
+++ Step219ClassAndMethods/Step219ClassAndMethods/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step219ClassAndMethods
+{
+    public class MathOperation
+    {
+        public static int MathOperator(int num)
+        {
+            return num + 10;
+        }
+
+        //add more integers via method overloading
+        public static int MathOperator(int num1, int num2)
+        {
+            return (num1 + num2) * 10;
+        }
+
+        //take in a string and convert it to an integer if possible
+        public static int MathOperator(int num1, int num2, string statement)
+        {
+            int converted = Convert.ToInt32(statement);
+            return (num1 + num2 + converted) % 10;
+        }
+        
+    }
+}
---
+++ Step219ClassAndMethods/Step219ClassAndMethods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step219ClassAndMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please pick a whole number.");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            int output = MathOperation.MathOperator(input);
+            Console.WriteLine("Your number plus 10 is: " + output);
+            Console.ReadLine();
+
+            Console.WriteLine("Please pick a second number.");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+            int output2 = MathOperation.MathOperator(input, input2);
+            Console.WriteLine("Your first number plus your second number multiplied by ten is: " + output2);
+            Console.ReadLine();
+
+            Console.WriteLine("Please pick a third number.");
+            string input3 = Console.ReadLine();
+            int output3 = MathOperation.MathOperator(input, input2, input3);
+            Console.WriteLine("Your first number plus your second number plus your third number divided by ten has a remainder of: " + output3);
+            Console.ReadLine();
+        }
+    }
+}

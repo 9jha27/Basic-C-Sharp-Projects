725bc85cb108a7e70468794de734c4841ea0a13b
9jha27 <65628178+9jha27@users.noreply.github.com>
2020-09-01 21:48:38 -0600

    Creating a void method that taeks two integers as parameters

---
+++ Step221ClassAndMethodsAssignment/Step221ClassAndMethodsAssignment/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step221ClassAndMethodsAssignment
+{
+    public class MathOperations
+    {
+        public static void Addition(int num1, int num2)
+        {
+            Console.WriteLine("Parameter 1 plus 5 is: " + (num1 + 5));
+            Console.WriteLine("Parameter 2 is: " + num2);
+        }
+    }
+}

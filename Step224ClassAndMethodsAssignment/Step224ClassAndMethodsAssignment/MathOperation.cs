cc2695bc9542f114bb02f1c6fcc0e9b93f3a71de
9jha27 <65628178+9jha27@users.noreply.github.com>
2020-09-02 14:39:14 -0600

    Outpur Parametrs, Method Overloading, and Declaring a static class

---
+++ Step224ClassAndMethodsAssignment/Step224ClassAndMethodsAssignment/MathOperation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Step224ClassAndMethodsAssignment
+{
+    public class MathOperation
+    {
+        public void Division(int num1)
+        {
+            Console.WriteLine("The number divided by 2 is: " + num1 / 2);
+        }
+
+        public void Division(double num1)
+        {
+            Console.WriteLine("The number divided by 2 is: " + num1 / 2);
+        }
+
+        public void newOp(out int x, out int y)
+        {
+            x = 50;
+            y = 60;
+        }
+
+    }
+    static class newOperation
+    {
+        public static string fName = "Laura";
+        public static string lName = "Ha";
+        public static void Name()
+        {
+            Console.WriteLine("This was written using a static class: " + fName + " " + lName);
+        }
+
+    }
+}
---
+++ Step224ClassAndMethodsAssignment/Step224ClassAndMethodsAssignment/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Step224ClassAndMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           MathOperation mathop = new MathOperation(); //instantiate a new object of the class MathOperation
+
+            //EXERCISE 1 - 4//
+            Console.WriteLine("Please enter a whole number."); //have the user enter a number
+            int input = Convert.ToInt32(Console.ReadLine()); //convert the input string to an int type
+
+            mathop.Division(input); //Call the method on the inputted number
+
+            Console.ReadLine(); //display the output on the screen
+
+            //EXERCISE 5 - OVERLOAD A METHOD//
+            Console.WriteLine("Please enter a number with a decimal.");
+            double input2 = Convert.ToDouble(Console.ReadLine());
+            mathop.Division(input2);
+
+            Console.ReadLine();
+
+            //OUT PARAMETER EXERCISE 4//
+            MathOperation outParameter = new MathOperation(); //Create a method with out Parameters.
+            int x, y; //local definition of variable
+            
+            outParameter.newOp(out x, out y);
+
+
+            Console.WriteLine("The first value out is: " + x);
+            Console.WriteLine("The second value out is: " + y);
+
+
+            //STATIC CLASS EXERCISE 6//
+            newOperation.Name();
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}

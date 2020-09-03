97e157aa5d3ecabb8315343ae89efe86fe74e0f1
9jha27 <65628178+9jha27@users.noreply.github.com>
2020-09-03 16:06:27 -0600

    Abstract Class Instantiating Objects Assignment

---
+++ Step237 AbstractClass/Step237 AbstractClass/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step237_AbstractClass
+{
+    public class Employee : Person //inherits from Person class
+    {
+        public override void SayName() //SayName() method implemented inside Employee class
+        {
+            firstName = "Sample";
+            lastName = "Student";
+            Console.WriteLine(firstName + " " + lastName);
+        }
+    }
+}
---
+++ Step237 AbstractClass/Step237 AbstractClass/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step237_AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee name = new Employee();
+            name.SayName();
+
+            Console.ReadLine();
+
+        }
+    }
+}

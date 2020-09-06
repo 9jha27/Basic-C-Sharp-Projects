2d13ff99fcb3a191192951b610b0ad21a19ba530
9jha27 <65628178+9jha27@users.noreply.github.com>
2020-09-05 22:50:49 -0600

    Enum Days Of Week Assignment. Very confusing, go over if there is time.

---
+++ EnumAssignment/EnumAssignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter the current day of the week.");
+
+            string input = Console.ReadLine();
+            try
+            {
+                DaysOfWeek day = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), input, true);
+                Console.WriteLine("It is a " + day + " today!");
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+
+            
+            Console.ReadLine();
+        }
+        public enum DaysOfWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+    }
+}

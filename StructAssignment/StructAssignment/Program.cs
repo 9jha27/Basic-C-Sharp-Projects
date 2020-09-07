aecfc59d8cbb46cb0f8dc1d79a1a455a47da6b26
9jha27 <65628178+9jha27@users.noreply.github.com>
2020-09-07 15:01:39 -0600

    Struct Assignment

---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number;
+            number.Amount = 3.265468856688M;
+
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+
+            
+        }
+
+        struct Number
+        {
+            public decimal Amount;
+        }
+    }
+}

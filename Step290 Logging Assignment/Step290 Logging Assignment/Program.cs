50d83ff4c1d7e970517629bebedc9aa46a2359af
9jha27 <65628178+9jha27@users.noreply.github.com>
2020-09-12 15:07:41 -0600

    Logging Assignment (File I/O)

---
+++ Step290 Logging Assignment/Step290 Logging Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step290_Logging_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please pick a number.");
+            string userInput = Console.ReadLine();
+            using (StreamWriter input = new StreamWriter(@"C:\Users\Jina\Desktop\log.txt", true))
+            {
+                input.WriteLine(userInput);
+            }
+            Console.WriteLine(userInput + " is now logged in your txt file. Thank you!");
+            Console.ReadLine();
+        }
+    }
+}

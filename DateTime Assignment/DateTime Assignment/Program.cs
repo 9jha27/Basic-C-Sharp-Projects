389b5e5e9af64d6d1eb2e50bbb8eae1fb6a549cf
9jha27 <65628178+9jha27@users.noreply.github.com>
2020-09-12 17:34:49 -0600

    DateTime Assignment

---
+++ DateTime Assignment/DateTime Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Enter a number.");
+            
+            double userInput = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("It will be " + DateTime.Now.AddHours(userInput) + " " + userInput + " hours from now."); 
+            Console.ReadLine();
+        }
+    }
+}

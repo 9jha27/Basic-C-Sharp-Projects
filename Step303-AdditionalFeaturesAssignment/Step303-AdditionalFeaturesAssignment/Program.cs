03a61de770e3d86dc8c12a01b2e849c639c46e28
9jha27 <65628178+9jha27@users.noreply.github.com>
2020-09-14 19:47:11 -0600

    Additional C# Features

---
+++ Step303-AdditionalFeaturesAssignment/Step303-AdditionalFeaturesAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step303_AdditionalFeaturesAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string fName = "Laura";
+            var daysOfWeek = new string[7]
+            {
+                "Sunday","Monday,","Tuesday","Wednesday","Thursday","Friday","Saturday"
+            };
+            var studentSchedule = new Schedule(fName, daysOfWeek[6]);
+            var studentSchedule2 = new Schedule(fName);
+
+            Console.WriteLine(studentSchedule + "\n" + studentSchedule2);
+            
+        }
+
+
+}   
+    
+    
+}
---
+++ Step303-AdditionalFeaturesAssignment/Step303-AdditionalFeaturesAssignment/Schedule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step303_AdditionalFeaturesAssignment
+{
+    public class Schedule
+    {
+        public Schedule(string name) : this(name, "Wednesday")
+        {
+        }
+
+        public Schedule(string name, string dayOfWeek)
+        {
+            Console.WriteLine(name + " is set to have an audition on " + dayOfWeek + ".");
+            Console.ReadLine();
+        }
+    }
+}
